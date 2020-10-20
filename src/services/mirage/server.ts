2e545ec823ecdc6438b26b30fa4f347868a43516
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-21 03:47:03 +0500

    Update server.ts

--- src/services/mirage/server.ts
+++ src/services/mirage/server.ts
@@ -1,4 +1,6 @@
 import { Server, Model, Factory, belongsTo, hasMany, Response } from 'miragejs';
+import user from './routes/user';
+import * as diary from './routes/diary';
 
 export const handleErrors = (error: any, message = 'An error ocurred') => {
     return new Response(400, undefined, {
@@ -43,7 +45,19 @@ export const setupServer = (env?: string): Server => {
 
        routes(): void {
            this.urlPrefix = 'https://diaries.app';
+
+           this.get('/diaries/entries/:id', diary.getEntries);
+           this.get('/diaries/:id', diary.getDiaries);
+
+           this.post('/auth/login', user.login);
+           this.post('/auth/signup', user.signup);
+
+           this.post('/diaries/', diary.create);
+           this.post('/diaries/entry/:id', diary.addEntry);
+
+           this.put('/diaries/entry/:id', diary.updateEntry);
+           this.put('/diaries/:id', diary.updateDiary);
        },
 
-    });
-}
+    });      
+};
