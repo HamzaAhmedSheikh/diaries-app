c1885f0e8b694415c8650d9e44f1c6602730678e
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-16 04:59:13 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './app/App';
 import { setupServer } from './services/mirage/server';
 import { Provider } from 'react-redux';
 import store from './store';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 
   if(process.env.NODE_ENV === 'development') {
